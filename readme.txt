5a27de326b1136c09dc482b3f37aa7fce0a400af
bannerweb <bannerweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-20 16:46:45 +0000

    git-svn-id: https://plugins.svn.wordpress.org/simple-twitter-status-updates/tags/1.1@278784 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== Plugin Name ===
+Contributors: bannerweb
+Tags: twitter, status, updates, update, bannerweb, post, posts, page, pages, comment, commented, automatically, automatic, tweet,
+Requires at least: 2.7
+Tested up to: 3.0
+Stable tag: 1.1
+
+Automatically publishes a status on your twitter account when a post has been plublished, modified or commented by an user.
+
+== Description ==
+
+<p>The &quot;Simple Twitter Status Updates&quot; plugin automatically publishes a status on your twitter account when a post has been plublished, modified or commented by an user.</p>
+<p>The &quot;Simple Twitter Status Updates&quot; plugin is configured in just a few easy clicks and gets connected with your twitter account using oAuth. That means: you dont need to commit your twitter password!</p>
+<p>Keep your follwers up-to-date with what happens on your blog!</p>
+
+== Frequently Asked Questions ==
+
+Do you have questions or issues with the &quot;Simple Twitter Status Updates&quot; plugin?
+
+Please follow us on twitter and ask your question: <a href="http://twitter.com/bannerweb">@bannerweb</a>
+
+
+== Installation ==
+1. Unzip and upload the &quot;simple-twitter-status-updates&quot; plugin folder to the /wp-content/plugins/ directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Authenticate the &quot;Simple Twitter Status Updates&quot; to allow automatically status updates on your twitter timeline
+4. Define the plugin settings (Settings -> Twitter Updates) as you like and watch it working!
+
+
+== Changelog ==
+
+= 1.1 =
+* Added the latest twitter-async class files
+* Improved compatibility check
+
+= 1.0.1 =
+* Minor bug fix
+
+= 1.0 =
+* First public release
---
+++ simple-twitter-status-updates.php
@@ -0,0 +1,94 @@
+<?php
+
+/*
+
+Plugin Name: Simple Twitter Status Updates
+Plugin URI: http://www.bannerweb.ch/unsere-leistungen/wordpress-dev/simple-twitter-status-updates/
+Description: Automatically publishes a status on your twitter account when a post has been plublished, modified or commented by an user.
+Version: 1.1
+Author: Bannerweb GmbH
+Author URI: http://www.bannerweb.ch/
+
+*/
+
+# Functions
+# -----------------------------------------------------
+
+// Display incompatibility notification
+function stsu_incompatibility_notification(){
+	
+	echo '<div id="message" class="error">
+	
+	<p><b>The &quot;Simple Twitter Status Updates&quot; plugin does not work on this wordpress installation!</b></p>
+	<p>Please check your installation for following minimum requirements:</p>
+	
+	<p>
+	- Wordpress version 2.7 or higer<br />
+	- PHP version 5.2 or higher<br />
+	- PHP extension CURL 7.0 or higher
+	</p>
+	
+	<p>Do you need help? Contact us on twitter <a href="http://twitter.com/bannerweb">@bannerweb</a></p>
+	
+	</div>';
+}
+
+# Compatibilty check / plugin initialization
+# -----------------------------------------------------
+
+// get wordpress version number and fill it up to 9 digits
+$int_wp_version = preg_replace('#[^0-9]#', '', get_bloginfo('version'));
+while(strlen($int_wp_version) < 9){
+	
+	$int_wp_version .= '0'; 
+}
+
+// get php version number and fill it up to 9 digits
+$int_php_version = preg_replace('#[^0-9]#', '', phpversion());
+while(strlen($int_php_version) < 9){
+	
+	$int_php_version .= '0'; 
+}
+
+// Check if CURL is loaded, get version number and fill it up to 9 digits
+if(extension_loaded('curl') === true){
+	
+	$arr_curl_version = curl_version();
+	$int_curl_version = preg_replace('#[^0-9]#', '', $arr_curl_version['version']);
+	while(strlen($int_curl_version) < 9){
+		
+		$int_curl_version .= '0'; 
+	}
+}
+
+// Check overall plugin compatibility
+if(	$int_wp_version >= 270000000 and 	// Wordpress version > 2.7
+	$int_php_version >= 520000000 and 	// PHP version > 5.2
+	$int_curl_version >= 700000000 and 	// CURL version > 7.0
+	defined('ABSPATH') and 				// Plugin is not loaded directly
+	defined('WPINC')){					// Plugin is not loaded directl
+
+	// Load class file
+	require_once(dirname(__FILE__).'/stsu.class.php');
+	
+	// Build admin menu
+	add_action('admin_menu', array('STSU', 'buildAdminMenu'), 1);
+	
+	// Register publish post action
+	add_action('publish_post', array('STSU', 'postPagePublish'), 10, 1);
+
+	// Register new popst comment action
+	add_action('comment_post', array('STSU', 'commentPublish'), 10, 2);
+	
+	// Register new popst comment action
+	add_action('edit_comment', array('STSU', 'commentEdit'), 10, 1);
+}
+
+// Plugin is not compatible with current configuration
+else{
+	
+	// Display incompatibility information
+	add_action('admin_notices', 'stsu_incompatibility_notification');
+}
+
+?>
